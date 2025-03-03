1360a42f8398c9d742715b45bfb618283300b198
Roger Florat Gutierrez <rflorat07@gmail.com>
2025-03-03 23:35:29 +0100

    Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'core/locator/service_locator.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   /// -- Initialize Service Locator
   await serviceLocatorInit();
+
+  /// -- Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const App());
 }
